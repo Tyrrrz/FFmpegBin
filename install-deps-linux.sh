0e100e902c16634151977f4c5a6a80d7a55ba0a2
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-06-24 23:11:24 +0300

    asd

--- install-deps-linux.sh
+++ install-deps-linux.sh
@@ -1,31 +1,31 @@
 #!/bin/sh -e
 
 # Allow :i386 architecture for 32-bit cross-compilation support
-dpkg --add-architecture i386
+sudo dpkg --add-architecture i386
 
 # Update package lists
-apt update
+sudo apt update
 
 # Cross-compilation support
-apt install linux-headers-$(uname -r)
-apt install linux-libc-dev:i386
-apt install libc6-dev-i386
-apt install gcc-multilib
-apt install gcc-aarch64-linux-gnu
-apt install g++-multilib
-apt install g++-aarch64-linux-gnu
+sudo apt install linux-headers-$(uname -r)
+sudo apt install linux-libc-dev:i386
+sudo apt install libc6-dev-i386
+sudo apt install gcc-multilib
+sudo apt install gcc-aarch64-linux-gnu
+sudo apt install g++-multilib
+sudo apt install g++-aarch64-linux-gnu
 
 # FFmpeg dependencies
-apt install autoconf
-apt install automake
-apt install libtool
-apt install libltdl-dev
-apt install libx11-dev
-apt install libxft-dev
-apt install libxext-dev
-apt install libwayland-dev
-apt install libxkbcommon-dev
-apt install libegl1-mesa-dev
-apt install libibus-1.0-dev
-apt install nasm
-apt install pkg-config
+sudo apt install autoconf
+sudo apt install automake
+sudo apt install libtool
+sudo apt install libltdl-dev
+sudo apt install libx11-dev
+sudo apt install libxft-dev
+sudo apt install libxext-dev
+sudo apt install libwayland-dev
+sudo apt install libxkbcommon-dev
+sudo apt install libegl1-mesa-dev
+sudo apt install libibus-1.0-dev
+sudo apt install nasm
+sudo apt install pkg-config
